c2d69d796e39fe8737305278cf64a1622fece5d8
KipchumbaIan <iankipchumba2@gmail.com>
2025-07-18 22:02:50 +0300

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -1,57 +1,16 @@
-// jest.config.js - Root Jest configuration file
-
 module.exports = {
-  // Base configuration for all tests
-  projects: [
-    // Server-side tests configuration
-    {
-      displayName: 'server',
-      testEnvironment: 'node',
-      testMatch: ['<rootDir>/server/tests/**/*.test.js'],
-      moduleFileExtensions: ['js', 'json', 'node'],
-      setupFilesAfterEnv: ['<rootDir>/server/tests/setup.js'],
-      coverageDirectory: '<rootDir>/coverage/server',
-      collectCoverageFrom: [
-        'server/src/**/*.js',
-        '!server/src/config/**',
-        '!**/node_modules/**',
-      ],
-    },
-    
-    // Client-side tests configuration
-    {
-      displayName: 'client',
-      testEnvironment: 'jsdom',
-      testMatch: ['<rootDir>/client/src/**/*.test.{js,jsx}'],
-      moduleFileExtensions: ['js', 'jsx', 'json'],
-      moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-        '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/client/src/tests/__mocks__/fileMock.js',
-      },
-      setupFilesAfterEnv: ['<rootDir>/client/src/tests/setup.js'],
-      transform: {
-        '^.+\\.(js|jsx)$': 'babel-jest',
-      },
-      coverageDirectory: '<rootDir>/coverage/client',
-      collectCoverageFrom: [
-        'client/src/**/*.{js,jsx}',
-        '!client/src/index.js',
-        '!**/node_modules/**',
-      ],
-    },
-  ],
-  
-  // Global configuration
-  verbose: true,
-  collectCoverage: true,
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   coverageThreshold: {
     global: {
-      statements: 70,
-      branches: 60,
+      branches: 70,
       functions: 70,
       lines: 70,
-    },
-  },
-  testTimeout: 10000,
-}; 
+      statements: 70
+    }
+  }
+}
